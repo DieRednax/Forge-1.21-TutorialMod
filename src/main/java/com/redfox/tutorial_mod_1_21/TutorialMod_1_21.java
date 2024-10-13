21f8fafad0e4c29cae216a1fef224d2c92325524
DieRednax <xt.delange@gmail.com>
2024-10-13 20:08:17 +0200

    Custom Items

--- src/main/java/com/redfox/tutorial_mod_1_21/TutorialMod_1_21.java
+++ src/main/java/com/redfox/tutorial_mod_1_21/TutorialMod_1_21.java
@@ -1,6 +1,9 @@
 package com.redfox.tutorial_mod_1_21;
 
 import com.mojang.logging.LogUtils;
+import com.redfox.tutorial_mod_1_21.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -31,6 +34,8 @@ public class TutorialMod_1_21 {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -44,7 +49,10 @@ public class TutorialMod_1_21 {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
